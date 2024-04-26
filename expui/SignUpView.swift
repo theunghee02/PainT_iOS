17c4367f72b7ac2c2258de60abba914a068ebaf2
SURI <snyeg28842@gmail.com>
2024-04-26 21:53:42 +0900

    feat: 회원 가입 페이지 구현 이전 스켈레톤 코드(동작 확인용)

---
+++ expui/SignUpView.swift
@@ -0,0 +1,19 @@
+//
+// Created by Shin Yeong Gwak on 2024/04/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Image("tmpPaintLogo")
+                        .padding(.top,80)
+            }
+            
+        }
+    }
+
+}
