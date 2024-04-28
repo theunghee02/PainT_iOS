33fd11c014a02b39c77d143503a5a3577aba3aaf
theunghee02 <theunghee02@gmail.com>
2024-04-29 01:41:43 +0900

    feat : decorate stacks && add a comment

--- PainT/Home/homeSwiftUIView.swift
+++ PainT/Home/homeSwiftUIView.swift
@@ -10,28 +10,41 @@ import SwiftUI
 struct homeSwiftUIView: View {
     var body: some View {
         NavigationView {
-            HStack {
-                VStack {
-                    Text("통증 기록")
-                        .font(.system(size: 18))
-                        .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/).padding(10)
-                    Text("내 몸의 통증을\n기록해서 확인하기")
-                        .font(.system(size: 14)).foregroundColor(Color(red: 0x6E / 255, green: 0x6E / 255, blue: 0x6E / 255))
-                        .padding([.leading, .bottom, .trailing], 10.0)
-                } // VStack
-                .background(Color(red: 0xD9 / 255, green: 0xD9 / 255, blue: 0xD9 / 255))
-                
-                VStack {
-                    Text("질환 예측")
-                        .font(.system(size: 18))
-                        .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/).padding(10)
-                    Text("통증을 종합해서\n질환 예측해보기")
-                        .font(.system(size: 14))
-                        .foregroundColor(Color(red: 0x6E / 255, green: 0x6E / 255, blue: 0x6E / 255))
-                        .padding([.leading, .bottom, .trailing], 10.0)
-                } // VStack
-                .background(Color(red: 0xD9 / 255, green: 0xD9 / 255, blue: 0xD9 / 255))
-            } // HStack
+            VStack {
+                Text("수정님, 또 허리가 아프지는 않으세요?")
+                    .font(.system(size: 18))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                HStack {
+                    VStack {
+                        Text("통증 기록")
+                            .font(.system(size: 18))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                            .padding(10)
+                        Text("내 몸의 통증을\n기록해서 확인하기")
+                            .font(.system(size: 14))
+                            .foregroundColor(Color(red: 0x6E / 255, green: 0x6E / 255, blue: 0x6E / 255))
+                            .padding([.leading, .bottom, .trailing], 10.0)
+                        Image("clipBoard")
+                    } // VStack
+                    .frame(width: 140, height: 160)
+                    .background(Color(red: 0xD9 / 255, green: 0xD9 / 255, blue: 0xD9 / 255))
+                    .cornerRadius(10)
+                    VStack {
+                        Text("질환 예측")
+                            .font(.system(size: 18))
+                            .fontWeight(/*@START_MENU_TOKEN@*/.semibold/*@END_MENU_TOKEN@*/)
+                            .padding(10)
+                        Text("통증을 종합해서\n질환 예측해보기")
+                            .font(.system(size: 14))
+                            .foregroundColor(Color(red: 0x6E / 255, green: 0x6E / 255, blue: 0x6E / 255))
+                            .padding([.leading, .bottom, .trailing], 10.0)
+                        Image("wand")
+                    } // VStack
+                    .frame(width: 140, height: 160)
+                    .background(Color(red: 0xD9 / 255, green: 0xD9 / 255, blue: 0xD9 / 255))
+                    .cornerRadius(10)
+                } // HStack
+            } // VStack
         }
     }
 }
