303da8fd85173c65759a0a796c97c6e18565ffce
theunghee02 <theunghee02@gmail.com>
2024-04-30 03:25:48 +0900

    feat : create painRecordInfoTrigger && register main color

---
+++ PainT/PainRecord/painRecordUnitySwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  painRecordUnitySwiftUIView.swift
+//  PainT
+//
+//  Created by 최승희 on 4/30/24.
+//
+
+import SwiftUI
+
+struct painRecordUnitySwiftUIView: View {
+    var body: some View {
+        Text("painRecordUnitySwiftUIView")
+    }
+}
+
+#Preview {
+    painRecordUnitySwiftUIView()
+}
