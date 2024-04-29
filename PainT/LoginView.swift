e31df1575400f054855d8469f2a876cb1a46df67
SURI <snyeg28842@gmail.com>
2024-04-29 17:30:10 +0900

    feat: 로그인 UI 생성, 로고 재생성 필요, 디자인 상관하지 않음

---
+++ PainT/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  expui
+//
+//  Created by Shin Yeong Gwak on 2024/04/25.
+//
+
+
+import SwiftUI
+
+struct LoginView: View {
+//    @EnvironmentObject var variable: Variable
+
+    var body: some View {
+        VStack {
+            VStack {
+                Image("tmpPaintLogo")
+                        .padding(.top,80)
+            }
+            
+        }
+    }
+    
+}
