99b6383f63f8e31cb57ce668c93a829b9cc1964d
theunghee02 <theunghee02@gmail.com>
2024-05-01 21:21:10 +0900

    feat : create loginSwiftUIView

---
+++ PainT/Login/loginSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  loginSwiftUIView.swift
+//  PainT
+//
+//  Created by 최승희 on 5/1/24.
+//
+
+import SwiftUI
+
+struct loginSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    loginSwiftUIView()
+}
