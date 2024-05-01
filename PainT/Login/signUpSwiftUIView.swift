fabc4d1883e25b732f2844fea931985977cdec39
SURI <snyeg28842@gmail.com>
2024-05-01 21:38:05 +0900

    feat: 로그인 관련 뷰, 포맷에 맞게 수정하여 커밋!

---
+++ PainT/Login/signUpSwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  signUpSwiftUIView.swift
+//  PainT
+//
+//  Created by Shin Yeong Gwak on 5/1/24.
+//
+
+import SwiftUI
+
+struct signUpSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    signUpSwiftUIView()
+}
