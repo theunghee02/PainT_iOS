d4a9a06485caad2e2384dd7b13d3ac273a3c4fa2
theunghee02 <theunghee02@gmail.com>
2024-05-05 17:38:16 +0900

    feat : create degree UI and onTapGesture

--- PainT/PainRecord/painRecordInfoDegreeSwiftUIView.swift
+++ PainT/PainRecord/painRecordInfoDegreeSwiftUIView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct painRecordInfoDegreeSwiftUIView: View {
+    
+    @State private var selectedDegree: Int?
+    
+    let degreeTextList = [
+        "통증 없음",
+        "",
+        "가벼운 통증",
+        "",
+        "보통 통증",
+        "",
+        "심한 통증",
+        "",
+        "매우 심한 통증",
+        "",
+        "최악의 통증"
+    ]
+    
     var body: some View {
         Text("통증의 강도는 어느 정도인가요?")
+            .padding(20)
+        HStack(spacing: 20) {
+            ForEach((0...10).filter { $0 % 2 == 0 }, id: \.self) { idx in
+                degreeRow(idx: idx, degreeText: degreeTextList[idx])
+            }
+        } // HStack
+    }
+    
+    func degreeRow(idx: Int, degreeText: String) -> some View {
+        VStack(spacing: 20) {
+            Text("\(idx)")
+                .background(
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(selectedDegree == idx ? Color("AccentColor") : Color.gray)
+                )
+                .foregroundColor(.black)
+                
+            Text("\(degreeText)")
+                .font(.system(size:12))
+                .foregroundColor(selectedDegree == idx ? Color("AccentColor") : Color.gray)
+        } // VStack
+        .onTapGesture {
+            self.selectedDegree = idx
+        }
     }
 }
 
