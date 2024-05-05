8188f6daa7d7c3fce83daa13036350a9b228cc2c
theunghee02 <theunghee02@gmail.com>
2024-05-06 01:28:00 +0900

    feat : create last page's bottomButton

--- PainT/PainRecord/painRecordResultSwiftUIView.swift
+++ PainT/PainRecord/painRecordResultSwiftUIView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct painRecordResultSwiftUIView: View {
     var body: some View {
-        Text("painRecordResultSwiftUIView")
+        Spacer()
+        HStack(spacing: 0) {
+            NavigationLink(destination: tabSwiftUIView().navigationBarBackButtonHidden()) {
+                Text("홈 돌아가기")
+                    .foregroundColor(.white)
+                    .frame(minHeight: 50)
+                    .frame(width: UIScreen.main.bounds.width * 0.4)
+                    .background(Color(red: 0xD9 / 255, green: 0xD9 / 255, blue: 0xD9 / 255))
+            }
+            NavigationLink(destination: DiseasePredictionSwiftUIView()) {
+                Text("질환 예측하기")
+                    .foregroundColor(.white)
+                    .frame(minHeight: 50)
+                    .frame(width: UIScreen.main.bounds.width * 0.6)
+                    .background(Color("AccentColor"))
+            }
+        } // HStack
     }
 }
 
