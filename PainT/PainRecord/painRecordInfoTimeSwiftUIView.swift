a14b21d4abe8400b650c3f23199b9a3f9750bee3
theunghee02 <theunghee02@gmail.com>
2024-05-05 18:22:57 +0900

    feat : create painRecordInfo's Time

---
+++ PainT/PainRecord/painRecordInfoTimeSwiftUIView.swift
@@ -0,0 +1,24 @@
+//
+//  painRecordInfoTimeSwiftUIView.swift
+//  PainT
+//
+//  Created by 최승희 on 5/5/24.
+//
+
+import SwiftUI
+
+struct painRecordInfoTimeSwiftUIView: View {
+    
+    @State private var selectedTime = Date()
+    
+    var body: some View {
+        Text("통증 발생 시간은 어떻게 되나요?")
+        DatePicker("시간 선택", selection: $selectedTime, in: ...Date(), displayedComponents: .hourAndMinute)
+            .datePickerStyle(CompactDatePickerStyle())
+            .labelsHidden()
+    }
+}
+
+#Preview {
+    painRecordInfoTimeSwiftUIView()
+}
