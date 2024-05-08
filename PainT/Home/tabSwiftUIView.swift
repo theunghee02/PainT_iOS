0566059e35649ce26fa59fea71b20a3c30f8fc89
theunghee02 <theunghee02@gmail.com>
2024-05-09 02:48:15 +0900

    fix : change color according to Tab in the TabView
    Xcode에서 제공하는 아이콘을 사용하지 않아 해당 탭이 선택되어도 accentColor로 변경되지 않는 문제 해결

--- PainT/Home/tabSwiftUIView.swift
+++ PainT/Home/tabSwiftUIView.swift
@@ -8,23 +8,28 @@
 import SwiftUI
 
 struct tabSwiftUIView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             // home
             homeSwiftUIView()
                 .tabItem {
-                    Image("homeGNB-default")
+                    Label("홈", image: selectedTab == 0 ? "homeGNB-colored" : "homeGNB-default")
                 }
+                .tag(0)
             // painStat
             painStatSwiftUIView()
                 .tabItem {
-                    Image("painStat-default")
+                    Label("통증 통계", image: selectedTab == 1 ? "painStat-colored" : "painStat-default")
                 }
+                .tag(1)
             // myPage
             myPageSwiftUIView()
                 .tabItem {
-                    Image("mypage-default")
+                    Label("마이페이지", image: selectedTab == 2 ? "mypage-colored" : "mypage-default")
                 }
+                .tag(2)
         } // TabView
     }
 }
