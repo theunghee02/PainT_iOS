2faacec92ee20375daf6f741604e8d4e3886c8b9
SURI <snyeg28842@gmail.com>
2024-05-11 13:30:19 +0900

    fix: 일단 유저 관련 리퀘스트만 써놓긴 했는데 voidRequest는 범용성있게 작성했기 때문에,,, response가 따로 없는 요청은 이걸 사용해도 무방할 듯 함!

---
+++ PainT/Request/UserService.swift
@@ -0,0 +1,75 @@
+//
+//  UserService.swift
+//  PainT
+//
+//  Created by Shin Yeong Gwak on 5/9/24.
+//
+
+import Alamofire
+import Foundation
+
+class UserService {
+    var apiPath :String
+    var hostUrl :String = "http://chi-iu.com"
+    
+    
+    init(apiPath: String) {
+        self.apiPath = apiPath
+    }
+    
+    // 로그인용 리퀘스트
+    public func loginRequest(parameters: Parameters,completion: @escaping (Result<(Response?, String?), Error>) -> Void) {
+        AF.request(hostUrl+apiPath, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .responseDecodable(of: Response.self) { response in
+                
+            switch response.result {
+            case .success(let value):
+                let cookies = HTTPCookie.cookies(withResponseHeaderFields: response.response?.allHeaderFields as? [String: String] ?? [:], for: response.request?.url ?? URL(string: "http://chi-iu.com")!)
+
+                            // 이름이 "REFRESH_TOKEN"이고 경로가 "/"인 쿠키를 찾음
+                            let refreshTokenCookie = cookies.first { $0.name == "REFRESH_TOKEN" && $0.path == "/" }
+                            // 해당 쿠키의 값을 리턴
+                            let refreshToken = refreshTokenCookie?.value
+                completion(.success((value,refreshToken)))
+                
+                
+            case .failure(let error):
+                print("실패다 : \(error)")
+                completion(.failure(error))
+            }
+                
+        }
+    }
+    
+    // 회원가입용 리퀘스트 username:String,password:String,realName:String,email:String,gender:Bool,birthday:Date,isAgreed4:Bool
+
+    public func voidRequest(parameters:Parameters,completion: @escaping (Result<Response, Error>) -> Void) {
+
+        AF.request(hostUrl+apiPath, method: .post, parameters: parameters, encoding: JSONEncoding.default)
+            .responseDecodable(of: Response.self) { response in
+                
+            switch response.result {
+            case .success(let value):
+                completion(.success(value))
+                
+            case .failure(let error):
+                print("실패다 : \(error)")
+                completion(.failure(error))
+            }
+                
+        }
+    }
+        
+}
+
+struct Response: Codable {
+    var isSuccess: Bool
+    var code: Int
+    var message: String
+    var result: AccessResult!
+}
+
+struct AccessResult: Codable {
+    let accessToken: String
+    let grantType: String
+}
