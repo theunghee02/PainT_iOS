d863e38a592028da2f48d6ef2a5478aedeb10fa7
theunghee02 <theunghee02@gmail.com>
2024-05-11 22:28:45 +0900

    fix : Correction of missing logo

--- PainT/Home/tabSwiftUIView.swift
+++ PainT/Home/tabSwiftUIView.swift
@@ -11,27 +11,29 @@ struct tabSwiftUIView: View {
     @State private var selectedTab = 0
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            // home
-            homeSwiftUIView()
-                .tabItem {
-                    Label("홈", image: selectedTab == 0 ? "homeGNB-colored" : "homeGNB-default")
-                }
-                .tag(0)
-            // painStat
-            painStatSwiftUIView()
-                .tabItem {
-                    Label("통증 통계", image: selectedTab == 1 ? "painStat-colored" : "painStat-default")
-                }
-                .tag(1)
-            // myPage
-            myPageSwiftUIView()
-                .tabItem {
-                    Label("마이페이지", image: selectedTab == 2 ? "mypage-colored" : "mypage-default")
-                }
-                .tag(2)
-        } // TabView
-        .navigationBarBackButtonHidden()
+        NavigationView {
+            TabView(selection: $selectedTab) {
+                // home
+                homeSwiftUIView()
+                    .tabItem {
+                        Label("홈", image: selectedTab == 0 ? "homeGNB-colored" : "homeGNB-default")
+                    }
+                    .tag(0)
+                // painStat
+                painStatSwiftUIView()
+                    .tabItem {
+                        Label("통증 통계", image: selectedTab == 1 ? "painStat-colored" : "painStat-default")
+                    }
+                    .tag(1)
+                // myPage
+                myPageSwiftUIView()
+                    .tabItem {
+                        Label("마이페이지", image: selectedTab == 2 ? "mypage-colored" : "mypage-default")
+                    }
+                    .tag(2)
+            } // TabView
+            .navigationBarBackButtonHidden()
+        } // NavigationView
     }
 }
 
