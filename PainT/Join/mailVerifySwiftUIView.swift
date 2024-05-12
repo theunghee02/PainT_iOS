18f6bd0c63c294763e0fbb79a4a7dd0fe17b2f39
SURI <snyeg28842@gmail.com>
2024-05-12 22:47:02 +0900

    feat: sign up 이 후 절차, is_enabled 항을 true로 만들기 위한 절차를 진행할 예정이고, 인증번호 6자리를 이용해 인증할 예정

---
+++ PainT/Join/mailVerifySwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  mailVerifySwiftUIView.swift
+//  PainT
+//
+//  Created by Shin Yeong Gwak on 5/9/24.
+//
+
+import SwiftUI
+
+struct mailVerifySwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mailVerifySwiftUIView()
+}
