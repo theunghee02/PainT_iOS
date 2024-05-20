2f66b4b53a8308f457055c43f82d9778a6868174
SURI <snyeg28842@gmail.com>
2024-05-20 14:33:03 +0900

    fix: 이거 안됨 WebDelegate일때 무슨 권한 문제같은게 있을지 살펴봐야함

--- PainT/Login/webLoginSwiftUiView.swift
+++ PainT/Login/webLoginSwiftUiView.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 import WebKit
 
-struct webLoginSwiftUiView: UIViewControllerRepresentable {
+struct webLoginSwiftUiView: UIViewControllerRepresentable  {
     typealias UIViewControllerType = AuthViewController
     
     @Binding var isPresented: Bool
-    @Binding var accessToken: String
+    
     
     var type: String
     var urlToLoad: String!
     
     
-    init(isPresented: Binding<Bool>, accessToken: Binding<String>, type: String) {
+    init(isPresented: Binding<Bool>, type: String) {
         _isPresented = isPresented
         self.type = type
-        _accessToken = accessToken
+    
         if(self.type == "google"){
             self.urlToLoad = "https://accounts.google.com/o/oauth2/auth?client_id=365828773390-3312e7gi9r312aebng0ma2v407aiehqm.apps.googleusercontent.com&redirect_uri=http://chi-iu.com/api/v1/users/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email+openid+https://www.googleapis.com/auth/userinfo.profile"
         }
@@ -29,7 +29,7 @@ struct webLoginSwiftUiView: UIViewControllerRepresentable {
     
     //override
     func makeUIViewController(context: Context) -> AuthViewController {
-        let viewController = AuthViewController(isPresented: $isPresented, accessToken: $accessToken)
+        let viewController = AuthViewController(isPresented: $isPresented)
         viewController.url = urlToLoad
         return viewController
     }
@@ -56,12 +56,12 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
     private var webView: WKWebView!
     var url: String = ""
     @Binding var isPresented: Bool
-    @Binding var accessToken: String
+    
+    @State var cycle: Int = 0
     
     
-    init(isPresented: Binding<Bool>, accessToken: Binding<String>) {
+    init(isPresented: Binding<Bool>) {
             _isPresented = isPresented
-            _accessToken = accessToken
             super.init(nibName: nil, bundle: nil)
         }
         
@@ -79,7 +79,6 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.navigationDelegate = self
-        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
         
     }
     
@@ -88,35 +87,45 @@ class AuthViewController: UIViewController, WKNavigationDelegate {
             return
         }
         let urlRequest = URLRequest(url: url)
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
+        print(urlRequest)
         webView.load(urlRequest)
+        print("load?")
     }
-    
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if let url = webView.url,
-           let jsonScript = """
-            document.querySelector('pre').textContent
-            """.data(using: .utf8) {
-            webView.evaluateJavaScript("document.querySelector('pre').textContent") { (value, error) in
-                if let jsonStr = value as? String,
-                   let jsonData = jsonStr.data(using: .utf8) {
-                    do {
-                        let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
-                        if let result = (jsonObject as? [String: Any])?["result"] as? [String: Any]{
-                            let accessToken = result["accessToken"] as? String
-                            UserDefaultsService().saveAccessToken(token: accessToken!)
-                            let refreshToken = result["refreshToken"] as? String
-                            UserDefaultsService().saveRefreshToken(token: refreshToken!)
-                            print("여기")
-                            self.isPresented = true
-                        }
-                    
-                    } catch {
-                        print("JSON 파싱 오류:", error)
-                    }
-                }
-            }
-        }
+    func webView(_ webView : WKWebView, didCommit: WKNavigation!) {
+        print("머가 되긴 한다")
     }
+//
+//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        if( cycle > 0){
+//            if let url = webView.url,
+//               let _ = """
+//            document.querySelector('pre').textContent
+//            """.data(using: .utf8) {
+//                webView.evaluateJavaScript("document.querySelector('pre').textContent") { (value, error) in
+//                    if let jsonStr = value as? String,
+//                       let jsonData = jsonStr.data(using: .utf8) {
+//                        do {
+//                            let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+//                            if let result = (jsonObject as? [String: Any])?["result"] as? [String: Any]{
+//                                let accessToken = result["accessToken"] as? String
+//                                UserDefaultsService().saveAccessToken(token: accessToken!)
+//                                let refreshToken = result["refreshToken"] as? String
+//                                UserDefaultsService().saveRefreshToken(token: refreshToken!)
+//                                print("여기")
+//                                self.isPresented = true
+//                            }
+//                            
+//                        } catch {
+//                            print("JSON 파싱 오류:", error)
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        cycle += 1
+//    }
 
     
 }
+
